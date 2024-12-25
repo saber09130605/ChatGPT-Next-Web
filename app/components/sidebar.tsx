bec5ffc1a5d2a3bf2def71e54ae3285a242df6d5
hunter <hunter@example.com>
2024-12-25 11:34:06 +0800

    隐藏仓库

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -306,7 +306,7 @@ export function SideBar(props: { className?: string }) {
                 />
               </Link>
             </div>
-            <div className={styles["sidebar-action"]}>
+            {/* <div className={styles["sidebar-action"]}>
               <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
                 <IconButton
                   aria={Locale.Export.MessageFromChatGPT}
@@ -314,7 +314,7 @@ export function SideBar(props: { className?: string }) {
                   shadow
                 />
               </a>
-            </div>
+            </div> */}
           </>
         }
         secondaryAction={
