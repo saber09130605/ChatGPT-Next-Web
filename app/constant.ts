66caf795c08b8472af2747e19a18c98344c78e1b
hunter <hunter@example.com>
2024-12-25 15:51:18 +0800

    修改面具

--- app/constant.ts
+++ app/constant.ts
@@ -307,7 +307,7 @@ const openaiModels = [
   // "gpt-4o-2024-08-06",
   "gpt-4o-2024-11-20",
   // "chatgpt-4o-latest",
-  "gpt-4o-mini",
+  // "gpt-4o-mini",
   "gpt-4o-mini-2024-07-18",
   // "gpt-4-vision-preview",
   // "gpt-4-turbo-2024-04-09",
