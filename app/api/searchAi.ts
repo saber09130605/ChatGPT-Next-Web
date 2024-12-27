bcc3442f2a64bda14649abf8f67a7e774849f89c
saber09130605 <1031703364@qq.com>
2024-12-27 10:10:05 +0800

    feat:openai联网调试

--- app/api/searchAi.ts
+++ app/api/searchAi.ts
@@ -1,4 +1,37 @@
 import { NextRequest } from "next/server";
+const tools = [
+  {
+    type: "function",
+    function: {
+      name: "general",
+      description: "search for factors",
+      parameters: {
+        type: "object",
+        properties: {
+          query: { type: "string", description: "The query to search." },
+        },
+        required: ["query"],
+      },
+    },
+  },
+  {
+    type: "function",
+    function: {
+      name: "news",
+      description: "Search for news",
+      parameters: {
+        type: "object",
+        properties: {
+          query: {
+            type: "string",
+            description: "The query to search for news.",
+          },
+        },
+        required: ["query"],
+      },
+    },
+  },
+];
 export async function searchAi(req: NextRequest) {
   const reqClone = req.clone();
   const cloneBody = await reqClone.json();
@@ -7,31 +40,32 @@ export async function searchAi(req: NextRequest) {
   console.log("zoomModel", zoomModel);
   console.log("lastMessages", lastMessages);
   if (zoomModel && zoomModel != "none" && lastMessages.role == "user") {
-    const content = lastMessages.content;
+    // const content = lastMessages.content;
     try {
-      const searXNGResponse = await fetch(
-        `${process.env.NEXT_SEARXNG_BASE_URL}/search?q=${encodeURIComponent(
-          content,
-        )}&category=${zoomModel}&format=json`,
-      );
-      const searXNGData = await searXNGResponse.json();
-      const results = searXNGData.results.slice(0, 5).map((item: any) => ({
-        title: item?.title || "",
-        link: item?.url || "",
-        snippet: item?.content || "",
-      }));
-      const messages = cloneBody.messages;
-      const afterMessages = [
-        ...messages,
-        {
-          role: "tool",
-          name: zoomModel,
-          content: JSON.stringify(results),
-        },
-      ];
-      cloneBody.messages = afterMessages;
+      // const searXNGResponse = await fetch(
+      //   `${process.env.NEXT_SEARXNG_BASE_URL}/search?q=${encodeURIComponent(
+      //     content,
+      //   )}&category=${zoomModel}&format=json`,
+      // );
+      // const searXNGData = await searXNGResponse.json();
+      // const results = searXNGData.results.slice(0, 5).map((item: any) => ({
+      //   title: item?.title || "",
+      //   link: item?.url || "",
+      //   snippet: item?.content || "",
+      // }));
+      // const messages = cloneBody.messages;
+      // const afterMessages = [
+      //   ...messages,
+      //   {
+      //     role: "tool",
+      //     name: zoomModel,
+      //     content: JSON.stringify(results),
+      //   },
+      // ];
+      // cloneBody.messages = afterMessages;
       delete cloneBody.zoomModel;
-      console.log("messages ", cloneBody.messages);
+      cloneBody.tools = tools;
+      cloneBody.tool_choice = "auto";
       // 创建新的请求对象
       const modifiedReq = new NextRequest(req.url, {
         method: req.method,
@@ -40,6 +74,8 @@ export async function searchAi(req: NextRequest) {
       });
 
       req = modifiedReq;
+      // const response = await requestOpenai(req);
+      // console.log("search ai response", response);
       return req;
     } catch (error) {
       return undefined;
