3d355c518ad8694bb1510e55a27bbd3a160dfdc6
saber09130605 <1031703364@qq.com>
2024-12-31 10:02:22 +0800

    feat:联网测试

--- app/api/openai.ts
+++ app/api/openai.ts
@@ -82,14 +82,12 @@ export async function handle(
     // list models
     if (subpath === OpenaiPath.ListModelPath && response.status === 200) {
       const resJson = (await response.json()) as OpenAIListModelResponse;
-      console.log("[OpenAI Route] response success ", resJson);
+      // console.log("[OpenAI Route] response success ", resJson);
       const availableModels = getModels(resJson);
       return NextResponse.json(availableModels, {
         status: response.status,
       });
     }
-    const resJson = (await response.json()) as OpenAIListModelResponse;
-    console.log("[OpenAI Route] response ", resJson);
 
     return response;
   } catch (e) {
