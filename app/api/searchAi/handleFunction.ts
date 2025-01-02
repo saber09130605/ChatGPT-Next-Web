fdc04b03bbc37e8840f7249d49e5cffb09a505ea
saber09130605 <1031703364@qq.com>
2025-01-02 17:19:21 +0800

    Revert "feat:抽离联网搜索功能"
    This reverts commit 5845bb7caa21e659c2ff3a68b8ca57b60e760276.

--- app/api/searchAi/handleFunction.ts
+++ app/api/searchAi/handleFunction.ts
@@ -1,6 +1,3 @@
-import { NextRequest } from "next/server";
-import { requestOpenai } from "../common";
-import { tools } from "./searchAiConstant";
 export async function news(query: any) {
   console.log(`正在使用查询进行新闻搜索: ${JSON.stringify(query)}`);
   try {
@@ -49,72 +46,5 @@ export async function general(query: any) {
   } catch (error) {
     console.error(`在 general 函数中捕获到错误: ${error}`);
     return `在 general 函数中捕获到错误: ${error}`;
-  }
-}
-export async function searchOpenAi(req: NextRequest) {
-  const reqClone = req.clone();
-  const cloneBody = await reqClone.json();
-  try {
-    const searchBody = {
-      model: cloneBody.model,
-      messages: [lastMessages],
-      max_tokens: 3000,
-      tools: tools,
-      tool_choice: "auto",
-    };
-    // 创建新的请求对象
-    const searchReq = new NextRequest(req.url, {
-      method: req.method,
-      headers: req.headers,
-      body: JSON.stringify(searchBody),
-    });
-    const response = await requestOpenai(searchReq);
-    const responseClone = response.clone(); // 克隆响应对象
-    const searchData = await responseClone.json();
-    delete cloneBody.zoomModel;
-    const modifiedMessages = [
-      ...cloneBody.messages,
-      searchData.choices[0].message,
-    ];
-    let calledCustomFunction = false;
-    if (searchData.choices[0].message.tool_calls) {
-      const toolCalls = searchData.choices[0].message.tool_calls;
-      const availableFunctions = {
-        general: general,
-        news: news,
-      };
-      for (const toolCall of toolCalls) {
-        const functionName = toolCall.function.name;
-        const functionToCall =
-          availableFunctions[functionName as keyof typeof availableFunctions];
-        const functionArgs = JSON.parse(toolCall.function.arguments);
-        let functionResponse;
-        if (functionName === "general") {
-          functionResponse = await functionToCall(functionArgs.query);
-        } else if (functionName === "news") {
-          functionResponse = await functionToCall(functionArgs.query);
-        }
-        modifiedMessages.push({
-          tool_call_id: toolCall.id,
-          role: "tool",
-          name: functionName,
-          content: functionResponse,
-        });
-        calledCustomFunction = true;
-      }
-      if (calledCustomFunction) {
-        cloneBody.messages = modifiedMessages;
-        const modifiedReq = new NextRequest(req.url, {
-          method: req.method,
-          headers: req.headers,
-          body: JSON.stringify(cloneBody),
-        });
-        return modifiedReq;
-      }
-    } else {
-      console.log("没有发现函数调用");
-    }
-  } catch (error) {
-    console.error(`在 searchOpenAi 函数中捕获到错误: ${error}`);
   }
 }
