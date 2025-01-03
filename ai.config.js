0680d77c7444c8623aae14c96786106961753883
saber09130605 <1031703364@qq.com>
2025-01-03 20:41:51 +0800

    feat:打印日志

--- ai.config.js
+++ ai.config.js
@@ -11,6 +11,9 @@ module.exports = {
       env: {
         NODE_ENV: "production",
       },
+      output: "/var/log/ai-web-tz/out.log", // 标准输出日志路径
+      error: "/var/log/ai-web-tz/error.log", // 标准错误日志路径
+      combine_logs: true, // 将 stdout 和 stderr 合并到一个日志文件
     },
   ],
 };
